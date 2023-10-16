7073e7bac78fbc3462c62c5e7dc3eefa9e12b4f5
kingkazuma7 <your.nasionasio7@gmail.com>
2023-10-16 10:39:22 +0900

    サービスコンテナ2

--- app/Http/Controllers/LifeCycleTestController.php
+++ app/Http/Controllers/LifeCycleTestController.php
@@ -13,8 +13,35 @@ class LifeCycleTestController extends Controller
         });
         
         $test = app()->make('lifeCycleTest');
-        dd($test, app());
         
-        dd(app());
+        //サービスコンテナなしのパターン
+        // $message = new Message();
+        // $sample = new Sample($message);
+        // $sample->run();
+        
+        //サービスコンテナありのパターン
+        app()->bind('sample', Sample::class);
+        $sample = app()->make('sample');
+        $sample->run();
+        
+        dd($test, app());
+    }
+}
+
+class Sample
+{
+    public $message;
+    public function __construct(Message $message) {
+        $this->message = $message;
+    }
+    public function run(){
+        $this->message->send();
+    }
+}
+
+class Message
+{
+    public function send(){
+        echo('メッセージ表示');
     }
 }
