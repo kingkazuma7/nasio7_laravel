db7680ca645aa5fd68fd06c11f31af976f39d50a
kingkazuma7 <your.nasionasio7@gmail.com>
2023-11-11 10:30:56 +0900

    57. 一覧 tailblocks

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -23,25 +23,27 @@ class OwnersController extends Controller
     
     public function index()
     {
-        $date_now = Carbon::now();
-        $date_parse = Carbon::parse(now());
-        echo $date_now->year;
-        echo $date_parse;
+        // $date_now = Carbon::now();
+        // $date_parse = Carbon::parse(now());
+        // echo $date_now->year;
+        // echo $date_parse;
         
-        $e_all = Owner::all();
-        $q_get = DB::table('owners')->select('name', 'created_at')->get();
+        // $e_all = Owner::all();
+        // $q_get = DB::table('owners')->select('name', 'created_at')->get();
         // $q_first = DB::table('owners')->select('name')->first();
         
-        $c_test = collect([
-            'name' => 'てすと'
-        ]);
+        // $c_test = collect([
+        //     'name' => 'てすと'
+        // ]);
         
         // var_dump($q_first);
         
         // dd('オーナー一覧テスト');
         // dd($e_all, $q_get, $q_first, $c_test);
+        
+        $owners = Owner::select('name', 'email', 'created_at')->get();
         return view('admin.owners.index',
-        compact('e_all', 'q_get'));
+        compact('owners'));
     }
 
     /**
