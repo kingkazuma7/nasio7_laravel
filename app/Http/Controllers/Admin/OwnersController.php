e74f0dad31449c31a37a653ce138117665a9f4ad
kingkazuma7 <your.nasionasio7@gmail.com>
2023-11-12 11:33:07 +0900

    58 オーナーページ create 新規作成
    ※npm run watchする

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -53,7 +53,7 @@ class OwnersController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.owners.create');
     }
 
     /**
--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -17,6 +17,10 @@
                 
                 <section class="text-gray-600 body-font">
                   <div class="container mx-auto">
+                    <div class="flex justify-end mb-4">
+                      <button onclick="location.href='{{ route('admin.owners.create') }}'" class="bg-yellow-200 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-400 rounded text-lg">新規登録する</button>
+                    </div>
+                    
                     <div class="lg:w-2/3 w-full mx-auto overflow-auto">
                       <table class="table-auto w-full text-left whitespace-no-wrap">
                         <thead>
