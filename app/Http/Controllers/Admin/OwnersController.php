d672278977b38d4976e35e21fcbaaa84c25aee67
kingkazuma7 <your.nasionasio7@gmail.com>
2023-11-14 11:55:24 +0900

    62. Edit編集

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -42,7 +42,7 @@ class OwnersController extends Controller
         // dd('オーナー一覧テスト');
         // dd($e_all, $q_get, $q_first, $c_test);
         
-        $owners = Owner::select('name', 'email', 'created_at')->get();
+        $owners = Owner::select('id', 'name', 'email', 'created_at')->get();
         return view('admin.owners.index',
         compact('owners'));
     }
@@ -102,7 +102,10 @@ class OwnersController extends Controller
      */
     public function edit($id)
     {
-        //
+        $owner = Owner::findOrFail($id);
+        // dd($owner);
+        
+        return view('admin.owners.edit', compact('owner'));
     }
 
     /**
