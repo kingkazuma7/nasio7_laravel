21d7b1516b632c474a1db7c1d9e350d2852063a8
kingkazuma7 <your.nasionasio7@gmail.com>
2023-11-15 10:48:40 +0900

    64. ソフトデリート
    viewからdestoryメソッドへ

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -80,7 +80,8 @@ class OwnersController extends Controller
         
         return redirect()
             ->route('admin.owners.index')
-            ->with('message', 'オーナー登録を実施しました。');
+            ->with(['message' => 'オーナー登録を実施しました。',
+            'status' => 'info']);
     }
 
     /**
@@ -125,7 +126,8 @@ class OwnersController extends Controller
         
         return redirect()
             ->route('admin.owners.index')
-            ->with('message', 'オーナー情報を更新しました');
+            ->with(['message' => 'オーナー情報を更新しました',
+            'status' => 'info']);
     }
 
     /**
@@ -136,6 +138,12 @@ class OwnersController extends Controller
      */
     public function destroy($id)
     {
-        //
+        // dd('削除');
+        Owner::findOrFail($id)->delete(); // ソフトデリート
+        
+        return redirect()
+            ->route('admin.owners.index')
+            ->with(['message' => 'オーナー情報を削除しました。',
+            'status' => 'alert']);
     }
 }
--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -18,7 +18,7 @@
                 <section class="text-gray-600 body-font">
                   <div class="container mx-auto">
                     {{-- フラッシュメッセージ --}}
-                    <x-flash-message status="info" />
+                    <x-flash-message status="session('status')" />
                     
                     <div class="flex justify-end mb-4">
                       <button onclick="location.href='{{ route('admin.owners.create') }}'" class="bg-yellow-200 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-400 rounded text-lg">新規登録する</button>
@@ -32,6 +32,7 @@
                             <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">メール</th>
                             <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">作成日</th>
                             <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
+                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
                           </tr>
                         </thead>
                         <tbody>
@@ -43,6 +44,13 @@
                             <td class="px-4 py-3">
                               <button onclick="location.href='{{ route('admin.owners.edit', ['owner' => $owner->id ])}}'" class="text-white bg-indigo-400 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-500 rounded ">編集する</button>
                             </td>
+                            <form id="delete_{{$owner->id}}" method="post" action="{{ route('admin.owners.destroy', ['owner' => $owner->id ] )}}">
+                              @csrf
+                              @method('delete')
+                              <td class="px-4 py-3">
+                                <a href="#" data-id="{{ $owner->id }}" onclick="deletePost(this)" class="text-white bg-red-400 border-0 py-2 px-4 focus:outline-none hover:bg-red-500 rounded ">削除</a>
+                              </td>
+                            </form>
                           </tr>
                           @endforeach
                         </tbody>
@@ -66,4 +74,14 @@
           </div>
       </div>
   </div>
+  
+  <script>
+    function deletePost(e) {
+      'use strict';
+      if (confirm('本当に削除してもいいですか？')) {
+        document.getElementById('delete_' + e.dataset.id).submit();
+      }
+    }
+  </script>
+  
 </x-app-layout>
