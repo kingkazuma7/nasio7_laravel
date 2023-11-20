9724c595578b872f0c441bf16fd4c81baa3c843c
kingkazuma7 <your.nasionasio7@gmail.com>
2023-11-20 14:24:34 +0900

    72. Shop リレーション 1対1
    Eloquent リレーション設定
    Laravel Tinkerで確認

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Shop;
 
 class Owner extends Authenticatable
 {
@@ -39,4 +40,9 @@ class Owner extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
+    public function shop()
+    {
+        return $this->hasOne(Shop::class);
+    }
 }
